fd7815c68cb65c4f6f743679dc930bdf1b132d8a
eye534rahul <rahulreddy11605@gmail.com>
2025-08-03 14:20:14 +0530

    Fix GitHub Pages routing issues
    - Switch from BrowserRouter to HashRouter for GitHub Pages compatibility
    - Add 404.html for SPA redirects
    - Update index.html with GitHub Pages routing script
    - Fix home page loading issues

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
@@ -7,15 +7,14 @@ import Navbar from './components/navbar';
 
 const App = () => (
   <>
-    
-    <BrowserRouter>
-    <Navbar/>
+    <HashRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
 
